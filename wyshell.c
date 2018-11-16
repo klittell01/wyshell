754ec6f3a340c2a3640c97094b2bae47b77fdc83
Kevin Littell <klittell@uwyo.edu>
2018-11-16 14:06:18 -0700

    got it mostly owkring

--- wyshell.c
+++ wyshell.c
@@ -29,91 +29,10 @@ struct Node{
         char *inFile, *outFile, *errFile;
 };
 
-struct Node * head;
-
-void pushToBack(struct Node ** headRef, char *data){
-    struct Node *newNode;
-    newNode = (struct Node*)calloc(1, sizeof(struct Node));
-    newNode->command = strdup(data);
-    newNode->next = (*headRef);
-    (*headRef) = newNode;
-}
-
-/*
-struct Node *head = NULL;
-struct Node *start = NULL;
-
-void insert(char * data, int num) {
-   //create a link
-   struct Node *link = (struct Node*) malloc(sizeof(struct Node));
-
-   link->command = strdup(data);
-   link->num = num;
-
-   //point it to old first node
-   link->next = head;
-
-   //point first to new first node
-   head = link;
-
-   if(start == NULL){
-     //  start = link;
-     //  printf("start command: %s, link command: %s\n",start->command, link->command );
-   }
-}
-*/
-
-void printData(struct Node *  myNode){
-    //struct Node * ptr = head;
-    while(myNode != NULL){
-        printf("data for this node is: %s\n", myNode->command);
-        myNode = myNode->prev;
-
-    }
-}
-
-/* Function to reverse the linked list */
-static void reverseList(struct Node** headRef)
-{
-    struct Node* prev   = NULL;
-    struct Node* current = *headRef;
-    struct Node* next = NULL;
-    while (current != NULL)
-    {
-        // Store next
-        next  = current->next;
-
-        // Reverse current node's pointer
-        current->next = prev;
-
-        // Move pointers one position ahead.
-        prev = current;
-        current = next;
-    }
-    *headRef = prev;
-}
-
-
-/*
-//display the list
-void printList() {
-
-   struct Node *ptr = head;
-
-   printf("\n[head] =>");
-   //start from the beginning
-   while(ptr != NULL) {
-      printf(" %s + %d =>",ptr->command, ptr->num);
-      ptr = ptr->next;
-   }
-
-   printf(" [null]\n");
-}
-*/
-/// TODO: well my linked list is working but backwards of what i would like
-// i can probably just reverse the order of something to straighten it out
-// it always assigns the last word to all the commands of each node, except when
-// i add them one at time manually
+char *tokens[]={ "QUOTE_ERROR", "ERROR_CHAR", "SYSTEM_ERROR",
+               "EOL", "REDIR_OUT", "REDIR_IN", "APPEND_OUT",
+               "REDIR_ERR", "APPEND_ERR", "REDIR_ERR_OUT", "SEMICOLON",
+               "PIPE", "AMP", "WORD" };
 
 
 /* Function to reverse the linked list */
@@ -156,13 +75,18 @@ void printList(struct Node *head)
       printf(" %s,", tmp->command);
       tmp = tmp->next;
   }
+  printf("\n");
 }
 
 int main (int argc, char * argv[]){
     char buf[256];
     int rtn;
     char *rtpt;
-    head = calloc(1, sizeof(struct Node));
+    bool dirOut = false;
+    bool dirIn = false;
+    bool dirErr = false;
+    bool appErr = false;
+    bool appOut = false;
     while(1){
         printf("$> ");
         rtpt = fgets(buf, 256, stdin);
@@ -177,68 +101,155 @@ int main (int argc, char * argv[]){
             continue;
         }
 
-        struct Node *node, *head;
-        node = NULL;//calloc(1, sizeof(struct Node));
+        struct Node *node;
+        node = NULL;
 
-        while(rtn != EOL){
-            printf("command is : %s\n", lexeme);
-            push(&node, lexeme);
-            rtn = parse_line(NULL);
-        }
-        reverse(&node);
-        printList(node);
-        // these work
-        //pushToBack(&node, "one");
-        //pushToBack(&node, "two");
-        //pushToBack(&node, "three");
+        bool rdOut = false;
+        bool rdIn = false;
+        bool rdErr = false;
+        bool rdDefined = false;
 
-        // got to get the first one and then i think the second so we can point
-        // backwards easily.
-        /*
-        while(rtn != EOL){
-            switch (rtn) {
-                case WORD:
+        bool background = false;
+        bool myError = false;
+        bool beginningOfCommand = true;
 
-                    ;
-                    struct Word *tmpWord;
-                    tmpWord = calloc(1, sizeof(struct Word));
-                    tmpWord->command = lexeme;
-                    tmpWord->prev = myPrev;
-
-                    rtn = parse_line(NULL);
-                    struct Word *nextWord;
-                    if(rtn != EOL){
-                        nextWord = calloc(1, sizeof(struct Word));
-                        tmpWord->next = nextWord;
-                    } else {
-                        tmpWord->next = NULL;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-        */
-
-/*
         while(rtn != EOL){
             switch(rtn){
                 case WORD:
-                    ;
-                    struct Word *tmpWord;
-                    tmpWord = calloc(1, sizeof(struct Word));
-                    printf("%s\n", lexeme);
+                    if(beginningOfCommand == true){
+                        printf(":--: %s\n", lexeme);
+                        beginningOfCommand = false;
+                        rdIn = false;
+                        rdOut = false;
+                        rdErr = false;
+                    } else {
+                        printf(" --: %s\n", lexeme);
+                        rdDefined = true;
+                    }
                     break;
                 case ERROR_CHAR:
-                    printf("Error character: %d\n", rtn);
                     break;
                 default:
-                    printf("Error: %d\n", rtn);
-                    break;
+                    if (strcmp(tokens[rtn %96], "PIPE") == 0){
+                        if(beginningOfCommand == true){
+                            break;
+                            myError = true;
+                        }
+                        printf(" |\n");
+                        beginningOfCommand = true;
+                        rdIn = false;
+                        rdOut = false;
+                        rdErr = false;
+                        rdDefined = false;
+                    } else if(strcmp(tokens[rtn %96], "SEMICOLON") == 0){
+                        printf(" ;\n");
+                        beginningOfCommand = true;
+                        rdIn = false;
+                        rdOut = false;
+                        rdErr = false;
+                        rdDefined = false;
+                    } else if(strcmp(tokens[rtn %96], "REDIR_OUT") == 0){
+                        if(beginningOfCommand == true){
+                            myError = true;
+                            printf("Ambiguous redirection\n");
+                            break;
+                        }
+                        if(rdOut == true){
+                            printf("Ambiguous redirection\n");
+                            myError = true;
+                            break;
+                        }
+                        printf(" >\n");
+                        dirOut = true;
+                        rdOut = true;
+                        rdDefined = false;
+                    } else if(strcmp(tokens[rtn %96], "REDIR_IN") == 0){
+                        if(beginningOfCommand == true){
+                            myError = true;
+                            printf("Ambiguous redirection\n");
+                            break;
+                        }
+                        if(rdIn == true){
+                            printf("Ambiguous redirection\n");
+                            myError = true;
+                            break;
+                        }
+                        printf(" <\n");
+                        dirIn = true;
+                        rdIn = true;
+                        rdDefined = false;
+                    } else if(strcmp(tokens[rtn %96], "REDIR_ERR") == 0){
+                        if(beginningOfCommand == true){
+                            myError = true;
+                            printf("Ambiguous redirection\n");
+                            break;
+                        }
+                        if(rdErr == true){
+                            printf("Ambiguous redirection\n");
+                            myError = true;
+                            break;
+                        }
+                        printf(" 2>\n");
+                        dirErr = true;
+                        rdErr = true;
+                        rdDefined = false;
+                    } else if(strcmp(tokens[rtn %96], "APPEND_OUT") == 0){
+                        if(beginningOfCommand == true){
+                            myError = true;
+                            printf("Ambiguous redirection\n");
+                            break;
+                        }
+                        if(rdOut == true){
+                            printf("Ambiguous redirection\n");
+                            myError = true;
+                            break;
+                        }
+                        printf(" >>\n");
+                        dirOut = true;
+                        rdOut = true;
+                        rdDefined = false;
+                    } else if(strcmp(tokens[rtn %96], "APPEND_ERR") == 0){
+                        if(beginningOfCommand == true){
+                            myError = true;
+                            printf("Ambiguous redirection\n");
+                            break;
+                        }
+                        if(rdErr == true){
+                            printf("Ambiguous redirection\n");
+                            myError = true;
+                            break;
+                        }
+                        printf(" 2>>\n");
+                        dirErr = true;
+                        rdErr = true;
+                        rdDefined = false;
+                    } else if (strcmp(tokens[rtn %96], "QUOTE_ERROR") == 0){
+                        myError = true;
+                        break;
+                    } else if (strcmp(tokens[rtn %96], "AMP") == 0){
+                        background = true;
+                    }
+                    //printf("%d: %s\n", rtn, tokens[rtn%96]);
             }
+            if(myError == true){
+                break;
+            }
+            push(&node, lexeme);
             rtn = parse_line(NULL);
+            if(rtn == EOL){
+                if(rdDefined == false){
+                    printf("No file defined for redirection\n");
+                    myError = true;
+                    break;
+                }
+            }
         }
-        */
+        if(myError == false){
+            printf(" --: EOL\n");
+        }
+        //reverse(&node);
+        //printList(node);
+
     }
 
     printf("my string is: %s\n", buf);
